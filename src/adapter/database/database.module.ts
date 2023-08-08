e2c3877202771284b75dbafef57e2267d92ab649
Victor <vveralop@gmail.com>
2023-08-08 12:26:46 -0400

    Ajustes Iniciales

---
+++ src/adapter/database/database.module.ts
@@ -0,0 +1,23 @@
+import { DynamooseModule } from 'nestjs-dynamoose';
+import { Module } from '@nestjs/common';
+
+@Module({
+	imports: [
+        DynamooseModule.forRoot({
+           local: process.env.IS_DDB_LOCAL === 'true',
+          //local: 'http://localhost:8000',
+          aws: { 
+            region: 'us-east-1',
+            accessKeyId: 'vveral',
+            secretAccessKey: 'vveral'
+          },
+          table: {
+            create: true, //process.env.IS_DDB_LOCAL === 'true', --Probar en ambiente distinto
+          },
+        }),
+	],
+	controllers: [],
+	providers: [],
+}
+)
+export class Database {}
---
+++ src/domain/product/services/select-all.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel, Model } from 'nestjs-dynamoose';
+import { Product, ProductKey } from '../models/product.model';
+
+@Injectable()
+export class SelectAllProductService {
+  constructor(
+    @InjectModel('product')
+    private readonly model: Model<Product, ProductKey>,
+  ) { }
+
+  async findAll() {
+    try {
+      return  await this.model.scan().exec()
+      //return await dynamoose.model.scan().exec()
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+}
